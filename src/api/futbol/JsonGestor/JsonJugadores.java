c081af638fff543268b0207d7b16d55388f5fc46
jhonemu <jhonmurillousuga33@gmail.com>
2016-04-09 23:09:26 -0500

    jsonJugadores escritura

--- src/api/futbol/JsonGestor/JsonJugadores.java
+++ src/api/futbol/JsonGestor/JsonJugadores.java
@@ -1,5 +1,7 @@
 package api.futbol.JsonGestor;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import org.codehaus.jettison.json.JSONArray;
@@ -8,37 +10,56 @@ import org.codehaus.jettison.json.JSONObject;
 
 import api.futbol.Services.InfJugador;
 import api.futbol.jugadasComplejas.JugadaCompleja;
+import api.futbol.jugadasPrimitivas.JugadaPrimitiva;
 import api.futbol.jugador.Arquero;
 import api.futbol.jugador.Delantero;
 
 public class JsonJugadores {
 	public void Escribe() {
-		/*JSONObject object = new JSONObject();
+		JSONObject object = new JSONObject();
 		JSONArray arqueros =  new JSONArray();
 		JSONArray delanteros = new JSONArray();
-		
-		int i = 0;
-		while(InfJugador.listaJugadores.size()>0){
+	
+		for(int i = 0;i<InfJugador.listaJugadores.size();i++){
 			JSONObject ob = new JSONObject();
-			JSONObject p = new JSONObject();
-			JSONArray jug = new JSONArray();
-			ArrayList<JugadaCompleja> jugadasc = new ArrayList<>();
-			if(InfJugador.listaJugadores.get(i) instanceof Arquero){
-				try {
-					ob.put("nombre", InfJugador.listaJugadores.get(i).getNombre());
-					ob.put("pocision", InfJugador.listaJugadores.get(i).getPosicion());
-					jugadasc = InfJugador.listaJugadores.get(i).getListaJugadas();
-					for(int j = 0; j < jugadasc.size();j++){
-						p.put("jugada", jugadasc.get(j).getNombre());
-					}
-				} catch (JSONException e) {
-					e.printStackTrace();
+			JSONArray aux = new JSONArray();
+			ArrayList<JugadaCompleja> JugadaC = new ArrayList<>();
+			try {
+				ob.put("Nombre", InfJugador.listaJugadores.get(i).getNombre());
+				ob.put("Posicion",InfJugador.listaJugadores.get(i).getPosicion());
+				JugadaC = InfJugador.listaJugadores.get(i).getListaJugadas();
+				for(int j = 0 ; j<JugadaC.size();j++){
+					JSONObject at = new JSONObject();
+					at.put("Jugada", JugadaC.get(j).getNombre());
+					aux.put(at);
 				}
+				ob.put("Jugadas Complejas",aux);
+				if(InfJugador.listaJugadores.get(i) instanceof Arquero){
+					ob.put("Tiempo Sin gol",((Arquero) InfJugador.listaJugadores.get(i)).getTiempoSinGoles());
+					ob.put("Dorsal",((Arquero) InfJugador.listaJugadores.get(i)).getDorsal());
+					delanteros.put(ob);
+				}
+				else if(InfJugador.listaJugadores.get(i) instanceof Delantero){
+					ob.put("Goles Marcados", (((Delantero) InfJugador.listaJugadores.get(i)).getGolesMarcados()));
+					ob.put("Dorsal", (((Delantero) InfJugador.listaJugadores.get(i)).getDorsal()));
+					arqueros.put(ob);
+				}
+			} catch (JSONException e) {
+				e.printStackTrace();
 			}
-			else if(InfJugador.listaJugadores.get(i) instanceof Delantero){
-				
-			}
-		}*/
+		}
+		try {
+			object.put("Arqueros", arqueros);
+			object.put("Delanteros", delanteros);
+			FileWriter fil = new FileWriter("C:\\Users\\Public\\Documents\\JsonJugadores.json");
+			fil.write(object.toString());
+			fil.flush();
+			fil.close();
+		} catch (JSONException | IOException e) {
+			
+			e.printStackTrace();
+		}
+		
 	}
 	
 	public void Lee(){
--- src/api/futbol/Services/InfJugador.java
+++ src/api/futbol/Services/InfJugador.java
@@ -7,6 +7,10 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import api.futbol.JsonGestor.JsonJugadasComplejas;
+import api.futbol.JsonGestor.JsonJugadores;
+import api.futbol.jugador.Arquero;
+import api.futbol.jugador.Delantero;
 import api.futbol.jugador.Jugador;
 
 @Path("/jugador")
@@ -16,6 +20,12 @@ public class InfJugador {
 	@Path("/consulta")
 	@Produces(MediaType.TEXT_PLAIN)
 	public String consultar(){
+		new JsonJugadasComplejas().Lee();
+		Delantero jug1 = new Delantero("Juan","Delantero",(short)0,(byte)11,JugComplejas.listaJugadasComplejas);
+		Arquero jug2 = new Arquero("Juan","Arquero",(short)0,(byte)171,JugComplejas.listaJugadasComplejas);
+		listaJugadores.add(jug1);
+		listaJugadores.add(jug2);
+		new JsonJugadores().Escribe();
 		return "holi";
 	}
 
